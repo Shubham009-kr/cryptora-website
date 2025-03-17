a46ec08cb71cd80b339827f91b35d9b336244467
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-03-17 23:27:37 +0530

    updated hero section and header, added signin functionality.

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ document.addEventListener("mousemove", (event)=>{
         const speed = element.getAttribute("data-speed");
         element.style.transform = `translate(${x * speed * 20}px, ${y * speed * 20}px)`;
     })
+})
 
+const signinButton = document.getElementById("signinButton");
+const signinPage = document.getElementById("signinPage");
+const closeSignIn = document.getElementById("closeSignIn");
+
+signinButton.addEventListener("click", function(){
+    signinPage.classList.remove("closeSignIn");
+    signinPage.classList.add("openSignIn");
+})
+
+closeSignIn.addEventListener("click", function(){
+    signinPage.classList.remove("openSignIn");
+    signinPage.classList.add("closeSignIn");
 })
